1c4fce09a192d98b8c4bf6fb4675b6abc24c4336
Ruan Victor dos Santos Rateira <101360876+ruanvsrateira@users.noreply.github.com>
2023-04-07 13:37:07 -0300

    config: ConfigModule to use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
+import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TodoModule, UserModule]
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ".env",
+        }),
+        AuthModule, TodoModule, UserModule
+    ]
 })
 export class AppModule { }
